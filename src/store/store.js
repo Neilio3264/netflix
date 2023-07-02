dec317db1042603e5d2eca58ef8736ec23056b92
Neilio3264 <neilrathod12@gmail.com>
2023-07-02 16:00:09 -0500

    Refactor of redux to fit application use

--- src/store/store.js
+++ src/store/store.js
@@ -1,8 +1,8 @@
-import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '@store/features/counter/counterSlice';
+import { configureStore } from "@reduxjs/toolkit";
+import userReducer from "@store/features/user/userSlice";
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
+    user: userReducer,
   },
 });
